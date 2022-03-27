68c23f452e29e180151f95a9ffe9cdf0bb78487d
yaxpaccruzjuarez <ypjuarez11@gmail.com>
2022-03-27 18:38:22 -0400

    commit tree 5fa76e41515faa5b6270a10495a5078df3c7885e

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your solution in this file!
+const employee ={["name"]: 'Sam'};
+
+
+function updateEmployeeWithKeyAndValue(employee,key,value){
+    employee["streetAddress"]= '11 Broadway'
+
+    const newEmployee ={...employee}
+newEmployee["streetAddress"]= '11 Broadway'
+
+    return employee
+}
+
